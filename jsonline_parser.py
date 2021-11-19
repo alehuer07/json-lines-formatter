28f321dbe013ff692de6588bf2b0f390c0ed181d
Alexis Huerta <alehuer90@gmail.com>
2021-11-19 12:51:11 -0800

    clean up comments

--- jsonline_parser.py
+++ jsonline_parser.py
@@ -21,15 +21,6 @@ class MyHandler(FileSystemEventHandler):
             file_size = -1
 
             os.rename(src, new_destination)
-            # while file_size != os.path.getsize(FOLDER_TO_TRACK):
-            #     file_size = os.path.getsize(FOLDER_DESTINATION)
-            #     time.sleep(1)
-            # while not file_done:
-            #     try:
-            #         os.rename(src, new_destination)
-            #         file_done = True
-            #     except:
-            #         return True
 
 
 event_handler = MyHandler()
@@ -44,12 +35,13 @@ except KeyboardInterrupt:
     observer.stop()
 observer.join()
 
-# def changeFormatting():
-#     x = input("Type in the name of a file you want to convert: ")
-#     fileToConvert = "./" + x
 
-#     with open(fileToConvert, "r") as f:
-#         json_data = json.load(f)
+def changeFormatting():
+    x = input("Type in the name of a file you want to convert: ")
+    fileToConvert = "./" + x
 
-#     with jsonlines.open("./json_lines.jl", "w") as writer:
-#         writer.write_all(json_data)
+    with open(fileToConvert, "r") as f:
+        json_data = json.load(f)
+
+    with jsonlines.open("./json_lines.jl", "w") as writer:
+        writer.write_all(json_data)
